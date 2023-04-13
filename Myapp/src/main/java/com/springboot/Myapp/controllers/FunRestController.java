973c49d24cab85f0e1b9d52c11946b6454d49538
Savannah-sun <yiqian.sun@tufts.edu>
2023-04-12 20:44:43 -0400

    changed the embedding server port and also the context path

--- Myapp/src/main/java/com/springboot/Myapp/controllers/FunRestController.java
+++ Myapp/src/main/java/com/springboot/Myapp/controllers/FunRestController.java
@@ -1,10 +1,21 @@
 package com.springboot.Myapp.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    //inject properties from applications
+    @Value("${author}")
+    private String authorName;
+    @Value("${book}")
+    private String bookName;
+    // expose new endpoint for shelf
+    @GetMapping("/shelf")
+    public String getABook(){
+        return "Author: " + authorName + " , Book: " + bookName;
+    }
 
     // expose "/" that return "Hello World"
     @GetMapping("/")
